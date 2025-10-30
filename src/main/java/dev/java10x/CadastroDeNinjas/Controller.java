96267072e3bfcb40af6698ea86c8e7adaf735006
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-10-30 18:16:51 -0300

    Criado a Controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Controller {
 
-    @GetMapping("/boas-vindas")
+    @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é a minha primeira mensagem nessa rota";
     }
